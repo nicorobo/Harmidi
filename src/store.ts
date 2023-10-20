ca4e951e2a8d29089e589d5387f3f4142a1c36b8
nicorobo <nickroberts404@gmail.com>
2023-10-20 09:57:29 -0500

    created schema for row settings

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,52 @@
 import { create } from 'zustand'
 import { keyboardConfigs, KeyboardConfig } from './keyboard-config'
 
+type RowType = 'chord' | 'note' | 'operator'
+type CommonRowSettings = {
+  type: RowType
+  channel: number
+  octave: number // Maybe should be transpose?
+  velocity: number
+}
+
+type NoteRowSettings = CommonRowSettings & {
+  scale?: string
+}
+
+type CommonChordRowSettings = CommonRowSettings & {
+  voicing?: string
+}
+
+type ScaleChordRowSettings = CommonChordRowSettings & {
+  scale?: string
+}
+
+type FamilyChordRowSettings = CommonChordRowSettings & {
+  family: string
+}
+
+type ChordRowSettings = ScaleChordRowSettings | FamilyChordRowSettings
+
+type RowSettings = (NoteRowSettings | ChordRowSettings)[]
+
 interface State {
   active: number[]
   keyboardConfig: KeyboardConfig
   keydown: (key: string) => void
   keyup: (key: string) => void
+  rowSettings: RowSettings
 }
 
 const useStore = create<State>()((set) => ({
   active: [],
   keyboardConfig: keyboardConfigs.USEnglish,
+  scale: { root: 'C', type: 'major' },
+  rowSettings: [
+    { type: 'chord', channel: 1, octave: 3, velocity: 100 },
+    { type: 'chord', channel: 1, octave: 3, velocity: 100, family: 'maj7' },
+    { type: 'note', channel: 1, octave: 3, velocity: 100 },
+    { type: 'note', channel: 1, octave: 3, velocity: 100 },
+  ],
   keydown: (key) =>
     set((state) => ({
       active: [...state.active, state.keyboardConfig.keyToId[key]],

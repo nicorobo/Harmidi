c3152a128c5ce19a6263e27d466030eca64ed49e
nicorobo <nickroberts404@gmail.com>
2024-02-16 15:02:00 -0800

    added steelpan preset

--- src/zone-instruments.ts
+++ src/zone-instruments.ts
@@ -6,13 +6,28 @@ const pianoFactory = () =>
       attack: 0.02,
       decay: 0.1,
       sustain: 0.3,
-      release: 10,
+      release: 1,
+    },
+  }).toDestination()
+const steelpanFactory = () =>
+  new Tone.PolySynth(Tone.Synth, {
+    oscillator: {
+      type: 'fatcustom',
+      partials: [0.2, 1, 0, 0.5, 0.1],
+      spread: 40,
+      count: 3,
+    },
+    envelope: {
+      attack: 0.001,
+      decay: 1.6,
+      sustain: 0,
+      release: 1.6,
     },
   }).toDestination()
 
 export const availableInstruments = [
   { id: 'piano', name: 'Piano', factory: pianoFactory },
-  { id: 'piano2', name: 'Piano2', factory: pianoFactory },
+  { id: 'steelpan', name: 'Steel Pan', factory: steelpanFactory },
 ]
 
 export type ZoneInstrument = {
